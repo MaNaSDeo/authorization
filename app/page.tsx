2c54bc1fde91bc11408b279aa7995f5a61447a49
manasdeo <rmnslogin@gmail.com>
2024-08-31 23:58:59 +0530

    husky setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Home() {
-  return <main>Home</main>;
+  const router = useRouter();
+
+  return (
+    <main id="landingPage">
+      <button className="gtLgnPage" onClick={() => router.push("/login")}>
+        Go To Login Page
+      </button>
+    </main>
+  );
 }
